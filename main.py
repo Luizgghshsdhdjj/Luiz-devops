27dca5462294cf794b0a4958a607686ef5e53648
Luizgghshsdhdjj <luizguilhermemachadomachado@gmail.com>
2026-04-19 21:51:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-
     return {"message": "Hello World"}
-
 
 @app.get("/status")
 def status():
-    return {"status": "ok"} 
+    return {"status": "ok", "mensagem": "API Luiz funcionando"}
+
+@app.get("/teste")
+def teste():
+    return {"teste": "G2"}
